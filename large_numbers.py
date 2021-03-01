477e31a8f3b1cd60c2b16e787d65d9236a8bb3ba
chaeminsoo <chaems97@gmail.com>
2021-03-01 20:11:02 +0900

    greedy

---
+++ large_numbers.py
@@ -0,0 +1,38 @@
+'''
+Q: make largest number using given list of number by adding them 'M' times. You can't add specific index's number 'K' times in a row
+   The size of the arry is 'N' 
+
+input condition:
+    1st line: N(2<= N <= 1,000) , M(1<= M <= 10,000) , K(1<= K <= 10,000) ; separated by ' '
+    2nd line: 'N' natural numbers ; separated by ' ' ; 1 <= natural numbers <= 10,000
+    K <= M
+output condition:
+    1st line: result
+'''
+import time
+
+n,m,k = map(int,input().split())
+data = list(map(int, input().split()))
+
+start_time = time.time()
+data.sort()
+first = data[n-1]
+second = data[n-2]
+#result = 0
+
+result = (m//(k+1))*(first*k+second) + (m%(k+1))*first
+
+'''
+while True:
+    for i in range(k):
+        if m == 0: break
+        result += first
+        m -= 1
+    if m == 0: break
+    result += second
+    m -= 1
+'''
+print(result)
+end_time = time.time()
+print('time: ',end_time-start_time)
+    
