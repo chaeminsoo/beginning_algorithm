795e7d3a1487c7e89a8a7cdab2b5028844184c5a
chaeminsoo <chaems97@gmail.com>
2021-03-11 22:13:29 +0900

    BFS

--- maze.py
+++ maze.py
@@ -1,6 +1,4 @@
-'''
-incompleted
-'''
+
 from collections import deque
 import time
 
@@ -9,15 +7,16 @@ maze=[]
 for i in range(n):
     maze.append(list(map(int,input())))
 
+
 start_time=time.time()
 
-dx=[0,0,-1,1]
-dy=[-1,1,0,0]
-
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
 def bfs(x,y):
     queue = deque()
     queue.append((x,y))
 
+    
     while queue:
         x,y = queue.popleft()
 
@@ -30,10 +29,10 @@ def bfs(x,y):
             if maze[nx][ny] == 0:
                 continue
             if maze[nx][ny] ==1:
-                maze[nx][ny] == maze[x][y] +1
+                maze[nx][ny] = maze[x][y] +1
                 queue.append((nx,ny))
     return maze[n-1][m-1]
 
-print(bfs(1,1))
+print(bfs(0,0))
 end_time=time.time()
 print('time:',end_time-start_time)
