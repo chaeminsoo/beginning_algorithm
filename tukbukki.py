ca3e8a9fb0138dcc77c5f9a57ce03d4d22abca0a
chaeminsoo <chaems97@gmail.com>
2021-03-18 21:10:35 +0900

    Binary_Search

--- tukbukki.py
+++ tukbukki.py
@@ -1,22 +1,36 @@
 import time
-
+import random
+'''
+tuk=[]
+for i in range(1000000):
+    tuk.append(random.randint(1,2000000000))
+m=random.randint(1,200)
+print(m)
+'''
 n,m =map(int,input().split())
 tuk = list(map(int,input().split()))
 
 st=time.time()
 
-tuk.sort(reverse=True)
-tuktall=tuk[0]
-for i in range(tuktall, 0, -1):
-
 def binary_search(array, target, start, end):
+    total = 0
     if start > end:
         return None
     mid = (start+end)//2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid-1)
-    else:
-        return binary_search(array, target, mid+1, end)
+    #result = total - (mid*len(array))
+    for i in array:
+        if i>mid:
+            total += i-mid
+        else:
+            pass
 
+    if total == target:
+        return mid
+    elif total > target:
+        return binary_search(array, target, mid+1, end)
+    else:
+        return binary_search(array, target, start, mid-1)
+    
+print(binary_search(tuk,m,0,max(tuk)))
+et=time.time()
+print('time:',et-st)
