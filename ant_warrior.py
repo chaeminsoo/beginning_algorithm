24eb1832345c1db60604410a66c7e1b9db1908ed
chaeminsoo <chaems97@gmail.com>
2021-03-20 22:26:07 +0900

    Dynamic

--- ant_warrior.py
+++ ant_warrior.py
@@ -3,4 +3,14 @@ import time
 n = int(input())
 Ks = list(map(int,input().split()))
 
-d=[0]*(n+1)
+st=time.time()
+
+d=[0]*101
+d[1]=Ks[0]
+d[2]=max(d[1],Ks[1])
+for i in range(3,n+1):
+    d[i] = max(d[i-1],d[i-2]+Ks[i-1])
+
+print(d[n])
+et=time.time()
+print('time:',et-st)
