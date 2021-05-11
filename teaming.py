2a56f31f9559db316044349505ad10f6985b50d6
chaeminsoo <chaems97@gmail.com>
2021-05-11 23:15:51 +0900

    Disjoint

---
+++ teaming.py
@@ -0,0 +1,39 @@
+import time
+
+def find_parent(parent,  x):
+    if parent[x] != x:
+        parent[x]= find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a= find_parent(parent,a)
+    b= find_parent(parent,b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+n,m = map(int,input().split())
+# 0~n,  m time
+
+student = [0]*(n+1)
+parent = [0]*(n+1)
+result=[]
+
+for i in range(n+1):
+    parent[i] = i
+    student[i] = i
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+
+    if a == 0:
+        union_parent(parent,b,c)
+    else:
+        if find_parent(parent,b) == find_parent(parent,c):
+            result.append('YES')
+        else:
+            result.append('NO')
+
+for i in result:
+    print(i)
