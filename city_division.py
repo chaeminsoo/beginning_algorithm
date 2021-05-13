86aa480e293f756dfb1db664a0d0dc631734e489
chaeminsoo <chaems97@gmail.com>
2021-05-13 18:01:23 +0900

    Incomplete

--- city_division.py
+++ city_division.py
@@ -1,6 +1,20 @@
 n,m = map(int,input().split())
 # n: number of houses, m: number of roads
 
+def find_parent(parent,  x):
+    if parent[x] != x:
+        parent[x]= find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a= find_parent(parent,a)
+    b= find_parent(parent,b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+
 INF = int(1e9)
 graph = [[INF]*(n+1) for _ in range(n+1)]
 
